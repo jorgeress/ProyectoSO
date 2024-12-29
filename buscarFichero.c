bfedd4ff385d5a0461d87cd02698c6eb329dbe0a
jorge <jorgegarcia.eso@gmail.com>
2024-12-29 17:39:09 +0100

    version final de verdad

--- buscarFichero.c
+++
@@ -1,9 +0,0 @@
-int BuscaFich(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, char *nombre) {
-    for (int i = 0; i < MAX_FICHEROS; i++) {
-        // Verificar si el nombre coincide y si el inodo no es NULL
-        if (directorio[i].dir_inodo != NULL_INODO && strcmp(directorio[i].dir_nfich, nombre) == 0) {
-            return i;  // Retornar índice del archivo encontrado
-        } 
-    }
-    return -1;  // Retornar -1 si no se encontró
-}
